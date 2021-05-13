7c1abe6dceb291799e43397c1c0ba6a8cf67adaa
Kaleb  Huddleston <nanny@kalebs-air.lan>
2021-05-12 21:20:34 -0700

    Spruce up the home comp with proj info and percentage complete

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsFacade } from '@bba/core-state';
 
 @Component({
   selector: 'bba-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  projects$ = this.projectsFacade.allProjects$;
 
-  constructor() { }
+  constructor(private projectsFacade: ProjectsFacade) {}
 
   ngOnInit(): void {
+    this.projectsFacade.loadProjects();
   }
-
 }
